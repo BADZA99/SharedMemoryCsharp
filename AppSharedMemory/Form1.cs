0841ab9a25a83968f8798f5dc17b55da3ba3aeba
papabn <pndiaye999@gmail.com>
2024-07-18 00:26:33 +0000

    chore: Add necessary packages and configuration files for code first

--- AppSharedMemory/Form1.cs
+++ AppSharedMemory/Form1.cs
@@ -12,9 +12,16 @@ namespace AppSharedMemory
 {
     public partial class Form1 : Form
     {
+        ServiceMetier.Service1Client service;
         public Form1()
         {
+            service = new ServiceMetier.Service1Client();
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ MetierSharedMemory/Model/BdSharedMemoryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+
+namespace MetierSharedMemory.Model
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class BdSharedMemoryContext:DbContext
+    {
+        public BdSharedMemoryContext():base("connSharedMemory") { }
+    }
+}
